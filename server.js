58fc3b9cb9cf7ddee925d760bd58a0aff7ba6348
joeychoi7624@gmail.com <43774005+Joeychoi7624@users.noreply.github.com>
2022-08-17 11:21:34 +0900

    [DELETE] useless ignore files

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const cors = require("cors");
 const morgan = require("morgan");
 require("dotenv").config();
 
-//const routes = require("./routes");
+const routes = require("./routes");
 
 const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-//app.use(routes);
+app.use(routes);
 
 app.get("/ping", (req, res) => {
     res.json({ message: "pong" });
