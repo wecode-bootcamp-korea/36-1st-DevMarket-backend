d75e549920ad21bd47db81e2774eeea4afdf5ca9
Saehan <jungsae@oregonstate.edu>
2022-08-24 17:56:53 +0900

    [Fix] PR 리뷰 수정

--- services/cartService.js
+++ services/cartService.js
@@ -1,6 +1,7 @@
 const cartDao = require("../models/cartDao");
 
 const getCartLists = async (userId) => await cartDao.getProductsList(userId);
+
 const addProduct = async(userId, productId, amount) => await cartDao.addProduct(userId, productId, amount);
 
 module.exports = {
